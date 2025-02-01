359eaf89041944115436d2c85b292b54325f2cd5
Samyle Claudia <oliveirasamyle2@gmail.com>
2025-02-01 11:16:23 -0300

    Verifições e adicionar nomes a lista

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,28 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// criar uma variável para armazenar a lista
+var amigos = [];
+// Função para adicionar nomes a lista
+function adicionarAmigo() {
+ let nomes = document.querySelector('input');
+ let nomes2 = nomes.value.trim() // o .trim() serve para excluir os espaços colocados entre no texto
+ console.log(nomes2)
+ if (nomes2 == ''){ // Alertar que foi adicionado um espaço vazio 
+    alert('Digite um nome, não é resgistrado espaços vazio');
+    nomes.value = ''; // limpar o espeaço 
+} else if (amigos.includes(nomes2) ){  // Verificar se o nome já existe na lista
+    alert(`Digite um nome diferente, o nome ${nomes2} já existe na lista`);
+    nomes.value = '';
+} else {
+        amigos.push(nomes2); // Adicionar o nome a lista
+        console.log(amigos);
+        nomes.value = '';
+        let listaAparente = document.querySelector('ul'); //Seleciona o local onde será apresentada a lista
+           listaAparente.innerHTML = ''; // vai limpar a lista anterior para a nova 
+        for (let i = 0; i < amigos.length; i++) { // Loop
+            const amigo = amigos[i]; // Obtém o nome do amigo atual. const -> cria uma váriavel de valor fixo
+            const listItem = document.createElement('li');
+            listItem.textContent = amigo; // Define o texto do <li> como o nome do amigo
+            // Adiciona o <li> dentro da lista <ul>
+            listaAparente.appendChild(listItem);
+        }
+}
+}
